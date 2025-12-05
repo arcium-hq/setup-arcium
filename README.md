4bac55219a5f7754da0166f9e68ad5138184f1d1
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2025-12-05 14:29:54 +0530

    fix: update to v0.5.1

--- README.md
+++ README.md
@@ -14,22 +14,22 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v3
-      - uses: arcium-hq/setup-arcium@v0.3.0
+      - uses: arcium-hq/setup-arcium@v0.5.1
         with:
           runner-arch-os: x86_64_linux
       - run: arcium build
         shell: bash
 ```
 
-This will use the default versions of Arcium, Anchor, Node.js, and Solana CLI tools, which are 0.3.0, 0.31.1, 20.18.0, and 2.1.20 respectively. It will run on the specified architecture and operating system (which is currently a mandatory parameter), the possible options are: `aarch64_linux`, `x86_64_linux`, `aarch64_macos`, `x86_64_macos`. Windows is not supported yet. You can also configure these versions like so:
+This will use the default versions of Arcium, Anchor, Node.js, and Solana CLI tools, which are 0.5.1, 0.32.1, 20.18.0, and 2.3.0 respectively. It will run on the specified architecture and operating system (which is currently a mandatory parameter), the possible options are: `x86_64_linux`, `aarch64_macos`. You can also configure these versions like so:
 
 ```yaml
 steps:
   - uses: actions/checkout@v3
-  - uses: arcium-hq/setup-arcium@v0.3.0
+  - uses: arcium-hq/setup-arcium@v0.5.1
     with:
-      anchor-version: '0.30.1'
-      solana-cli-version: '2.2.0'
+      anchor-version: '0.32.1'
+      solana-cli-version: '2.3.0'
       node-version: '20.11.0'
       runner-arch-os: x86_64_linux
 ```
--- test_project/programs/test_project/src/lib.rs
+++ test_project/programs/test_project/src/lib.rs
@@ -3,14 +3,14 @@ use arcium_anchor::prelude::*;
 
 const COMP_DEF_OFFSET_ADD_TOGETHER: u32 = comp_def_offset("add_together");
 
-declare_id!("QuMnUzyu2HYQYD4dpBidz45DatqtZXSjAB4RfSYq93R");
+declare_id!("J5ZL1JFms1kQmsyw3AxpdPgcPRn4tYHRBN44q2ZDqZgf");
 
 #[arcium_program]
 pub mod test_project {
     use super::*;
 
     pub fn init_add_together_comp_def(ctx: Context<InitAddTogetherCompDef>) -> Result<()> {
-        init_comp_def(ctx.accounts, true, 0, None, None)?;
+        init_comp_def(ctx.accounts, None, None)?;
         Ok(())
     }
 
@@ -19,36 +19,41 @@ pub mod test_project {
         computation_offset: u64,
         ciphertext_0: [u8; 32],
         ciphertext_1: [u8; 32],
-        pub_key: [u8; 32],
+        pubkey: [u8; 32],
         nonce: u128,
     ) -> Result<()> {
         ctx.accounts.sign_pda_account.bump = ctx.bumps.sign_pda_account;
-        let args = vec![
-            Argument::ArcisPubkey(pub_key),
-            Argument::PlaintextU128(nonce),
-            Argument::EncryptedU8(ciphertext_0),
-            Argument::EncryptedU8(ciphertext_1),
-        ];
+        let args = ArgBuilder::new()
+            .x25519_pubkey(pubkey)
+            .plaintext_u128(nonce)
+            .encrypted_u8(ciphertext_0)
+            .encrypted_u8(ciphertext_1)
+            .build();
 
         queue_computation(
             ctx.accounts,
             computation_offset,
             args,
             None,
-            vec![AddTogetherCallback::callback_ix(&[])],
+            vec![AddTogetherCallback::callback_ix(
+                computation_offset,
+                &ctx.accounts.mxe_account,
+                &[]
+            )?],
+            1,
+            0,
         )?;
-
         Ok(())
     }
 
     #[arcium_callback(encrypted_ix = "add_together")]
     pub fn add_together_callback(
         ctx: Context<AddTogetherCallback>,
-        output: ComputationOutputs<AddTogetherOutput>,
+        output: SignedComputationOutputs<AddTogetherOutput>,
     ) -> Result<()> {
-        let o = match output {
-            ComputationOutputs::Success(AddTogetherOutput { field_0 }) => field_0,
-            _ => return Err(ErrorCode::AbortedComputation.into()),
+        let o = match output.verify_output(&ctx.accounts.cluster_account, &ctx.accounts.computation_account) {
+            Ok(AddTogetherOutput { field_0 }) => field_0,
+            Err(_) => return Err(ErrorCode::AbortedComputation.into()),
         };
 
         emit!(SumEvent {
@@ -80,19 +85,19 @@ pub struct AddTogether<'info> {
     pub mxe_account: Account<'info, MXEAccount>,
     #[account(
         mut,
-        address = derive_mempool_pda!()
+        address = derive_mempool_pda!(mxe_account, ErrorCode::ClusterNotSet)
     )]
     /// CHECK: mempool_account, checked by the arcium program.
     pub mempool_account: UncheckedAccount<'info>,
     #[account(
         mut,
-        address = derive_execpool_pda!()
+        address = derive_execpool_pda!(mxe_account, ErrorCode::ClusterNotSet)
     )]
     /// CHECK: executing_pool, checked by the arcium program.
     pub executing_pool: UncheckedAccount<'info>,
     #[account(
         mut,
-        address = derive_comp_pda!(computation_offset)
+        address = derive_comp_pda!(computation_offset, mxe_account, ErrorCode::ClusterNotSet)
     )]
     /// CHECK: computation_account, checked by the arcium program.
     pub computation_account: UncheckedAccount<'info>,
@@ -102,7 +107,7 @@ pub struct AddTogether<'info> {
     pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
     #[account(
         mut,
-        address = derive_cluster_pda!(mxe_account)
+        address = derive_cluster_pda!(mxe_account, ErrorCode::ClusterNotSet)
     )]
     pub cluster_account: Account<'info, Cluster>,
     #[account(
@@ -126,6 +131,16 @@ pub struct AddTogetherCallback<'info> {
         address = derive_comp_def_pda!(COMP_DEF_OFFSET_ADD_TOGETHER)
     )]
     pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+    #[account(
+        address = derive_mxe_pda!()
+    )]
+    pub mxe_account: Account<'info, MXEAccount>,
+    /// CHECK: computation_account, checked by arcium program via constraints in the callback context.
+    pub computation_account: UncheckedAccount<'info>,
+    #[account(
+        address = derive_cluster_pda!(mxe_account, ErrorCode::ClusterNotSet)
+    )]
+    pub cluster_account: Account<'info, Cluster>,
     #[account(address = ::anchor_lang::solana_program::sysvar::instructions::ID)]
     /// CHECK: instructions_sysvar, checked by the account constraint
     pub instructions_sysvar: AccountInfo<'info>,
